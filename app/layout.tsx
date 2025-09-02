c05d4ae3fe8eb2014c0004a05d57f1879ab02edc
Maksymilian Yakovlev <maksimilian199@gmail.com>
2025-09-02 12:38:16 +0300

    small fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-function isPalindrome(x) {
-  // store the original number
-  let num = x;
-
-  // if number is negative return false
-  if (x < 0) return false;
-
-  let ans = 0;
-  while (num > 0) {
-    const last_digit = num % 10;
-    ans = ans * 10 + last_digit;
-    num = Math.floor(num/ 10);
-  }
-
-  return x === ans;
-}
